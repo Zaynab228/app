9f9e7098672ec1aba8fee7ed0f93eb6ae8eccbfc
zaynab228 <ail>
2025-08-04 19:25:03 +0300

    second_commit

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,17 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
+const router = inject(Router);
+
+  
+    if (localStorage.getItem('tokenApp')) {
+      return true;  
+    } else {
+      
+       router.parseUrl('/login');
+    return false;
+    
+  }
   return true;
 };
--- src/app/guards/confirm-saving-form.guard.ts
+++ src/app/guards/confirm-saving-form.guard.ts
@@ -1,5 +1,11 @@
 import { CanDeactivateFn } from '@angular/router';
+import { SignUpComponent } from '../components/sign-up/sign-up.component';
 
 export const confirmSavingFormGuard: CanDeactivateFn<unknown> = (component, currentRoute, currentState, nextState) => {
-  return true;
-};
+ if(component instanceof SignUpComponent){
+    if(component.signUpObjectForm.dirty){
+      return window.confirm("there are some shanges not saved,are you sureyou want to leave")
+    }
+  }
+  //ok ==true== go to components
+  return true;};
